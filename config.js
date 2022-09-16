b86abb97cae8ae44164963eda21d1d40b3fa91c5
Tyrone <tvriesde@gmail.com>
2022-09-16 16:01:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var password = 'lol2342dfasdfas';
-var token = 'cfgvbhnjkfmsdafds-fdsafasf-234fsd-fdsafsad';
+var password = "lol2342dfasdfas";
+var token = "cfgvbhnjkfmsdafds-fdsafasf-234fsd-fdsafsad";
